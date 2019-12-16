14227d0b636efd9521fbbd07271fadb7f19c2b6b
Yang Liu <liuyang12@focusmedia.cn>
2019-12-16 15:51:34 +0800

    update

--- hello_smpl.py
+++ hello_smpl.py
@@ -50,10 +50,10 @@ m = load_model('models/basicModel_f_lbs_10_207_0_v1.0.0.pkl')
 
 ## Assign random pose and shape parameters
 # m.pose[:] = np.random.rand(m.pose.size) * .2
-m.pose[:] = np.ones_like(m.pose.size) * .2
+m.pose[:] = -np.ones_like(m.pose.size) * .2
 
 # m.betas[:] = np.random.rand(m.betas.size) * .03
-m.betas[:] = np.ones_like(m.betas.size) * .03
+m.betas[:] = -np.ones_like(m.betas.size) * .03
 
 print('m.pose.size: ' + str(m.pose.size))
 print('m.betas.size: ' + str(m.betas.size))
